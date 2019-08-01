2ce4ba77e379a9bb31b22a2a976aee6f2420ea07
douglas-cavalcante <henriquedouglas2803@gmail.com>
2019-08-01 16:22:02 -0300

    Atualiza configurações do app.js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 import 'dotenv/config';
 
 import express from 'express';
-
+import path from 'path';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
 
-import routes from './routes';
-import sentryConfig from './config/sentry';
-
 import './database';
+import sentryConfig from './config/sentry';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -17,14 +16,19 @@ class App {
 
     Sentry.init(sentryConfig);
 
+    this.server.use(Sentry.Handlers.requestHandler());
     this.middlewares();
     this.routes();
     this.exceptionHandler();
   }
 
   middlewares() {
-    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: false }));
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
@@ -36,11 +40,13 @@ class App {
     this.server.use(async (err, req, res, next) => {
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
+
         return res.status(500).json(errors);
       }
+
       return res.status(500).json({ error: 'Internal server error' });
     });
   }
 }
 
-export default new App().server;
+export default new App();
